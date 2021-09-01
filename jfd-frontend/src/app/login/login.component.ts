c9ad7869fae70d599ca255a19808e9bbec4529bf
Amar Husain <amar.husain156@gmail.com>
2021-09-01 19:29:47 +0300

    09-01-21-class changes

---
+++ jfd-frontend/src/app/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+    loginForm: FormGroup;
+
+    constructor(private fb: FormBuilder) {
+        this.loginForm = this.fb.group({
+            email:[''],
+            password: ['']
+        });
+     }
+
+    ngOnInit(): void {
+       
+    }
+
+    login(){
+
+        console.log(this.loginForm.value);
+        
+    }
+
+
+}
