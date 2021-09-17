1808c76cff417f460c23688f70bff80e72aa7d1d
Amar Husain <amar.husain156@gmail.com>
2021-09-17 16:50:25 +0300

    signup page

--- jfd-frontend/src/app/signup/signup.component.ts
+++ jfd-frontend/src/app/signup/signup.component.ts
@@ -1,15 +1,65 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { RegisterService } from '../register.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
 export class SignupComponent implements OnInit {
+  signUpForm: FormGroup;
+  mobilePattern = '^[0-9_-]{10,12}';
+  emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private registerService: RegisterService
+  ) {
+    this.signUpForm = this.fb.group({
+      firstname: [
+        '',
+        [
+          Validators.minLength(3),
+          Validators.required,
+          Validators.maxLength(15),
+        ],
+      ],
+      lastname: [
+        '',
+        [
+          Validators.minLength(3),
+          Validators.required,
+          Validators.maxLength(15),
+        ],
+      ],
+      dob: ['', [Validators.required]],
+      gender: ['Select...', [Validators.required]],
+      mobile: [
+        '',
+        [Validators.required, Validators.pattern(this.mobilePattern)],
+      ],
+      email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
+      // firstname: [''],
+    });
   }
 
+  ngOnInit(): void {}
+
+  signUp() {
+    // console.log(this.signUpForm.value);
+    this.registerService
+      .registerNewUser(this.signUpForm.value)
+      .subscribe((data) => {});
+  }
+
+  get f() {
+    return this.signUpForm.controls;
+  }
+  get firstName() {
+    return this.signUpForm.get('firstname');
+  }
+  get mobile() {
+    return this.signUpForm.get('mobile');
+  }
 }
