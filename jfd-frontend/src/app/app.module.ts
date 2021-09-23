2656526b932100dd547f81ad161a1a722afc699d
Amar Husain <amar.husain156@gmail.com>
2021-09-23 15:41:50 +0300

    removed unnecessary import

--- jfd-frontend/src/app/app.module.ts
+++ jfd-frontend/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
